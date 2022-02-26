6780efae86388e831e15b775cd2226300345a9b4
BaizaOP <zayn.baig@gmail.com>
2022-02-26 00:36:49 -0500

    added a couple more upgrade buttons and drastically changed js

--- script.js
+++ script.js
@@ -1,47 +1,56 @@
-const upgrade = document.querySelector('#upgrade1'),
+const upgrades = document.querySelector('#upgrades'),
       click = document.querySelector("#click"),
-      display = document.querySelector("#display");
+      display = document.querySelector("#display"),
+      SHOW_THRESHOLD = [20, 100, 400, 800, 1500],
+      COSTS = SHOW_THRESHOLD.map(e => e * 2.5),
+      INCS = [5, 20, 60, 120, 200];
 
 let counter = 0, // the amount of clicks displayed on the screen
     inc = 1,     // how many clicks you get per click of the button
-    unlockMilestone = 20, // how many clicks you have to click through to see the hidden button
-    cost = setCost(unlockMilestone), // how many clicks you have to have to buy the upgrade
-    collected = false; // have you collected the uppgrade
+    index = 0;
 
-upgrade.disabled = true; // initially set the upgrade button as disabled
+updateText();
 
-
-
+upgrades.childNodes.forEach((el) => {
+  if(el.nodeName == "BUTTON") {
+    el.disabled = true; 
+    el.addEventListener('click', (e) => {
+      if(index == retIndex(el.id) && counter >= COSTS[index]) { 
+        counter -= COSTS[retIndex(el.id)];
+        inc = INCS[index]; // level up! 
+        index++;
+        el.disabled = true;
+        updateText();
+      }
+    });
+  }
+});
+      
 click.addEventListener('click', (e) => {
   // everytime you click, increment the counter and update the text accordingly
   counter+=inc;
   updateText();
-  if(counter == unlockMilestone && !collected) {
-    upgrade.innerText = `Get Clicker Upgrade: ${cost}`;
-  } else if (counter == cost && !collected) {
-    upgrade.disabled = false;
+  if(counter >= SHOW_THRESHOLD[index]) {
+    upgrades.childNodes.forEach((e) => {
+      if(e.nodeName == "BUTTON" && retIndex(e.id) == index) {
+        e.innerText = `${COSTS[index]} Clicks: ${INCS[index]} inc`;
+      }
+    });
+  }
+  if(counter >= COSTS[index]) {
+    upgrades.childNodes.forEach((e) => {
+      if(e.nodeName == "BUTTON" && retIndex(e.id) == index) {
+        e.disabled = false;
+      }
+    });
   }
 });
-
-upgrade.addEventListener('click', (e)=> {
-  if(counter >= cost) { // this check is a lil redundant because the button only gets "undisabled" if you have enough clicks
-                        // but could be useful for later
-    inc += 4;
-    counter -= cost;
-    updateText();
-    collected = true;
-   
-    upgrade.innerText = "Collected!";
-    upgrade.disabled = true;
-  }
-});
-
-// maybe we can work with this somewhere else...
-function setCost(unlockMilestone) {
-  return unlockMilestone * 2.5;
-}
 
 // abstracted this away, yeah
 function updateText() {
-  display.innerText = `Clicks: ${counter}`;
+  display.innerText = `Clicks: ${counter}\nInc: ${inc}`;
+}
+
+function retIndex(str) {
+  return parseInt(str.slice(7));
 }
